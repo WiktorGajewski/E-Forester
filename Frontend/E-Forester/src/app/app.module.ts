9242b69e911c9539ccdb3a873a4714f27c6fd96f
WiktorGajewski <66271811+WiktorGajewski@users.noreply.github.com>
2021-11-23 17:32:18 +0100

    start work on frontend - first components

--- Frontend/E-Forester/src/app/app.module.ts
+++ Frontend/E-Forester/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
