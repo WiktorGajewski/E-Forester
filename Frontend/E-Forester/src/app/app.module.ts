615f52e4aaafe3c099321e8d7d366b4b5e11bfa3
WiktorGajewski <66271811+WiktorGajewski@users.noreply.github.com>
2021-11-26 19:35:04 +0100

    change favicon, my-theme and create login component

--- Frontend/E-Forester/src/app/app.module.ts
+++ Frontend/E-Forester/src/app/app.module.ts
@@ -1,26 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 const material = [
   MatToolbarModule,
   MatSidenavModule,
   MatButtonModule,
   MatIconModule,
-  MatListModule
+  MatListModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatInputModule
 ]
 
 @NgModule({
@@ -36,7 +43,7 @@ const material = [
     BrowserAnimationsModule,
     material,
     LayoutModule,
-    
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/E-Forester/src/app/login/login.component.ts
+++ Frontend/E-Forester/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  hide = true;
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm= new FormGroup({
+      login: new FormControl("", Validators.required),
+      password: new FormControl("", Validators.required)
+    });
+  }
+
+  login(formValues: any): void {
+    console.log("Próba logowania");
+    console.log("Login: " + formValues.login);
+    console.log("Hasło: " + formValues.password);
+  }
+
+  cancel(): void {
+    this.router.navigate(["/"]);
   }
 
 }
