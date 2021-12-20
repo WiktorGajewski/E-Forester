0b46cfdd64ff0bb33114768a5c6d92cdb487aae6
WiktorGajewski <66271811+WiktorGajewski@users.noreply.github.com>
2021-12-20 16:10:42 +0100

    subarea-list add paginator

--- Frontend/E-Forester/src/app/subareas/subarea-list.component.ts
+++ Frontend/E-Forester/src/app/subareas/subarea-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { tap } from 'rxjs';
 import { SubareaDataSource } from '../services/subareas/subarea.data-source';
 import { SubareaService } from '../services/subareas/subarea.service';
 
@@ -7,17 +9,34 @@ import { SubareaService } from '../services/subareas/subarea.service';
   templateUrl: './subarea-list.component.html',
   styleUrls: ['./subarea-list.component.css']
 })
-export class SubareaListComponent implements OnInit {
+export class SubareaListComponent implements OnInit, AfterViewInit {
   dataSource !: SubareaDataSource;
   displayedColumns = ["id", "address", "area"];
+
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
 
   constructor(private subareaService : SubareaService) { 
 
   }
-
+  
   ngOnInit(): void {
     this.dataSource = new SubareaDataSource(this.subareaService);
     this.dataSource.loadSubareas();
   }
 
+  ngAfterViewInit(): void {
+    this.paginator.page
+      .subscribe(
+        this.loadPage()
+      );
+  }
+
+  loadPage() : void {
+    this.dataSource.loadSubareas(
+      "",
+      "asc",
+      this.paginator.pageIndex,
+      this.paginator.pageSize
+    );
+  }
 }
