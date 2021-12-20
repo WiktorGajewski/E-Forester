ccfd0fe740c86d246c3cff9b9a43a1dfdfdd4d75
WiktorGajewski <66271811+WiktorGajewski@users.noreply.github.com>
2021-12-20 15:41:04 +0100

    subarea-list add spinner, fix duplicate class spinner

--- Frontend/E-Forester/src/app/services/subareas/subarea.data-source.ts
+++ Frontend/E-Forester/src/app/services/subareas/subarea.data-source.ts
@@ -1,11 +1,14 @@
 import { CollectionViewer, DataSource } from "@angular/cdk/collections";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, delay, finalize, Observable } from "rxjs";
 import { ISubarea } from "src/app/models/subarea.model";
 import { SubareaService } from "./subarea.service";
 
 export class SubareaDataSource implements DataSource<ISubarea> {
 
     private subareaSubject = new BehaviorSubject<ISubarea[]>([]);
+    private loadingSubject = new BehaviorSubject<boolean>(false);
+
+    public loading = this.loadingSubject.asObservable();
 
     constructor(private subareaService : SubareaService) {
 
@@ -17,10 +20,15 @@ export class SubareaDataSource implements DataSource<ISubarea> {
 
     disconnect(collectionViewer: CollectionViewer): void {
         this.subareaSubject.complete();
+        this.loadingSubject.complete();
     }
 
-    loadSubareas() : void {
+    loadSubareas(filter = "", sortDirection = "asc", pageIndex = 0, pageSize = 3) : void {
+
+        this.loadingSubject.next(true);
+
         this.subareaService.getSubareas()
+            .pipe(finalize(() => this.loadingSubject.next(false)))
             .subscribe({
                 next: (value: ISubarea[]) => {
                     this.subareaSubject.next(value);
