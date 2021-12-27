43c6e80f31f9ed73bd6bdaac749659f3f9aec8c5
WiktorGajewski <66271811+WiktorGajewski@users.noreply.github.com>
2021-12-27 16:39:23 +0100

    base charts examples

--- Frontend/E-Forester/src/app/components/cockpit/cockpit.component.ts
+++ Frontend/E-Forester/src/app/components/cockpit/cockpit.component.ts
@@ -15,12 +15,19 @@ export class CockpitComponent implements OnInit {
 
   public barChartLabels = ["2011","2012","2013","2014","2015","2016","2017"];
   public barChartType : ChartType = "bar";
+  public doughnutChartType : ChartType = "doughnut";
+  public radarChartType : ChartType = "radar";
+  public pieChartType : ChartType = "pie";
+
   public barChartLegend = true;
 
   public barChartData = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: "Series A"},
     {data: [28, 48, 40, 19, 36, 57, 20], label: "Series B"},
+  ];
 
+  public chartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: "Example"},
   ];
 
   constructor() { }
